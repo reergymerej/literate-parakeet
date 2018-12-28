8d2ede479e7d53bcce30213baf5a7a7eee99b984
Jeremy Greer <jex.grizzle@gmail.com>
2018-12-28 11:28:19 -0500

    Solve

--- elixir/rotational-cipher/rotational_cipher.exs
+++ elixir/rotational-cipher/rotational_cipher.exs
@@ -10,18 +10,18 @@ defmodule RotationalCipher do
   def rotate(text, shift) do
     text
     |> to_charlist()
-    |> Enum.map(fn(char) ->
-      shift_char(char, shift)
-      end)
+    |> Enum.map(fn(char) -> shift_char(char, shift) end)
     |> to_string()
   end
 
-
-  defp shift_char(char, shift) when (char + shift > ?z) do
+  defp shift_char(char, _) when (char < ?A) do char end
+  defp shift_char(char, _) when (char > ?z) do char end
+  defp shift_char(char, _) when (char > ?Z and char < ?a) do char end
+  defp shift_char(char, shift) when (char <= ?Z and char + shift > ?Z) do
+    ?A + (char + shift) - ?Z - 1
+  end
+  defp shift_char(char, shift) when (char <= ?z and char + shift > ?z) do
     ?a + (char + shift) - ?z - 1
   end
-
-  defp shift_char(char, shift) do
-    char + shift
-  end
+  defp shift_char(char, shift) do char + shift end
 end

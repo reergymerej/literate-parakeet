8cb6b10ba8670577eee4c8938eb60e16c934ee6a
Jeremy Greer <jex.grizzle@gmail.com>
2018-12-30 06:36:50 -0500

    Use helpers and check strings

--- elixir/bob/bob.exs
+++ elixir/bob/bob.exs
@@ -1,38 +1,35 @@
 defmodule Bob do
   def hey(input) do
     cond do
+      is_silence?(input) -> "Fine. Be that way!"
 
-      # if you ask him a question.  Bob answers 'Sure.'
-      input === "Does this cryogenic chamber make me look fat?"
-        -> "Sure."
-      input === "4?"
-        -> "Sure."
+      is_question?(input) and is_shouting?(input) -> "Calm down, I know what I'm doing!"
 
-      # if you yell a question at him.  He answers 'Calm down, I know what I'm doing!'
-      input === "THIS ISN'T SHOUTING?"
-        -> "Calm down, I know what I'm doing!"
+      is_question?(input) -> "Sure."
 
-      # if you address him without actually saying anything.  He says 'Fine. Be that way!'
-      input === ""
-        -> "Fine. Be that way!"
-      input === "  "
-        -> "Fine. Be that way!"
+      is_shouting?(input) -> "Whoa, chill out!"
 
-      # if you yell at him.  He answers 'Whoa, chill out!'
-      input === "WATCH OUT!"
-        -> "Whoa, chill out!"
-      input === "1, 2, 3 GO!"
-        -> "Whoa, chill out!"
-      input === "ZOMG THE %^*@#$(*^ ZOMBIES ARE COMING!!11!!1!"
-        -> "Whoa, chill out!"
-      input === "I HATE YOU"
-        -> "Whoa, chill out!"
-      input === "УХОДИ"
-        -> "Whoa, chill out!"
-
-      # to anything else.  He answers 'Whatever.'
-      true
-        -> "Whatever."
+      true -> "Whatever."
     end
+  end
+
+  defp is_silence?(text) do
+    String.trim(text) == ""
+  end
+
+  defp is_shouting?(text) do
+    Kernel.!( is_talking_in_caps?(text) )
+    and String.upcase(text) == text
+    or String.ends_with?(text, "!")
+  end
+
+  defp is_talking_in_caps?(text) do
+    text
+    |> String.split()
+    |> Enum.all?(&(String.capitalize(&1) == &1))
+  end
+
+  defp is_question?(text) do
+    String.ends_with?(text, "?")
   end
 end

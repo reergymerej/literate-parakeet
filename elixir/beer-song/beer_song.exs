730d29f49ea6609235554546e673b862637c3a2a
Jeremy Greer <jex.grizzle@gmail.com>
2019-03-04 19:08:17 -0500

    Less DRY, more clear

--- elixir/beer-song/beer_song.exs
+++ elixir/beer-song/beer_song.exs
@@ -3,51 +3,41 @@ defmodule BeerSong do
   Get a single verse of the beer song
   """
   @spec verse(integer) :: String.t()
-  def verse(number) when (number == 0) do
+
+  def verse(0) do
     """
-    #{get_line(number)}
+    No more bottles of beer on the wall, no more bottles of beer.
     Go to the store and buy some more, 99 bottles of beer on the wall.
+    """
+  end
+
+  def verse(1) do
+    """
+    1 bottle of beer on the wall, 1 bottle of beer.
+    Take it down and pass it around, no more bottles of beer on the wall.
+    """
+  end
+
+  def verse(2) do
+    """
+    2 bottles of beer on the wall, 2 bottles of beer.
+    Take one down and pass it around, 1 bottle of beer on the wall.
     """
   end
 
   def verse(number) do
     """
-    #{get_line(number)}
-    Take #{next_beer_pronoun(number - 1)} down and pass it around, #{beer_address(number - 1)} of beer on the wall.
+    #{number} bottles of beer on the wall, #{number} bottles of beer.
+    Take one down and pass it around, #{number - 1} bottles of beer on the wall.
     """
   end
 
-  defp get_line(beer_number) do
-    "#{beer_address(beer_number)} of beer on the wall, #{beer_address(beer_number)} of beer."
-    |> String.capitalize()
-  end
-
-  defp next_beer_pronoun(number) when (number > 0) do
-    "one"
-  end
-
-  defp next_beer_pronoun(number) when (number == 0) do
-    "it"
-  end
-
-  defp beer_address(number) when (number > 1) do
-    "#{number} bottles"
-  end
-
-  defp beer_address(number) when (number == 1) do
-    "#{number} bottle"
-  end
-
-  defp beer_address(number) when (number == 0) do
-    "no more bottles"
-  end
 
   @doc """
   Get the entire beer song for a given range of numbers of bottles.
   """
   @spec lyrics(Range.t()) :: String.t()
   def lyrics(range \\ 99..0) do
-    Enum.reduce(range, [], fn (number, acc) -> acc ++ [verse(number)] end)
-    |> Enum.join("\n")
+    Enum.map_join(range, "\n", &(verse(&1)))
   end
 end

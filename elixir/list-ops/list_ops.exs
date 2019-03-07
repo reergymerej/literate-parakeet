e46437f9041fb8fa4576fe4d7dad4483bc70dda7
Jeremy Greer <jex.grizzle@gmail.com>
2019-03-06 21:35:42 -0500

    Start list ops

---
+++ elixir/list-ops/list_ops.exs
@@ -0,0 +1,76 @@
+defmodule ListOps do
+  # Please don't use any external modules (especially List or Enum) in your
+  # implementation. The point of this exercise is to create these basic
+  # functions yourself. You may use basic Kernel functions (like `Kernel.+/2`
+  # for adding numbers), but please do not use Kernel functions for Lists like
+  # `++`, `--`, `hd`, `tl`, `in`, and `length`.
+
+  @spec count(list) :: non_neg_integer
+  def count(l) do
+    count(0, l)
+  end
+
+  defp count(acc, []) do
+    acc
+  end
+
+  defp count(acc, [head | tail]) do
+    count(acc + 1, tail)
+  end
+
+  @spec reverse(list) :: list
+  def reverse(l) do
+    reverse([], l)
+  end
+
+  defp reverse(acc, []) do
+    acc
+  end
+
+  defp reverse(acc, [head | tail]) do
+    reverse([head] ++ acc, tail)
+  end
+
+  @spec map(list, (any -> any)) :: list
+  def map(l, f) do
+    map([], l, f)
+  end
+
+  defp map(acc, [], f) do
+    acc
+  end
+
+  defp map(acc, [head | tail], f) do
+    map(acc ++ [f.(head)], tail, f)
+  end
+
+  @spec filter(list, (any -> as_boolean(term))) :: list
+  def filter(l, f) do
+    filter([], l, f)
+  end
+
+  defp filter(acc, [], f) do
+    acc
+  end
+
+  defp filter(acc, [head | tail], f) do
+    if f.(head) do
+      filter(acc ++ [head], tail, f)
+    else
+      filter(acc, tail, f)
+    end
+  end
+
+  @type acc :: any
+  @spec reduce(list, acc, (any, acc -> acc)) :: acc
+  def reduce(l, acc, f) do
+  end
+
+  @spec append(list, list) :: list
+  def append(a, b) do
+  end
+
+  @spec concat([[any]]) :: [any]
+  def concat(ll) do
+  end
+end
